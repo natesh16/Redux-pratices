87b98452cee05ef871400374848655a253572f13
Natesh <natesh0917@gmail.com>
2024-02-20 20:38:05 +0530

    redux react setup

--- redux/src/Customeradd.js
+++ redux/src/Customeradd.js
@@ -6,10 +6,8 @@ export default function Customeradd() {
   return (
     <div>
     <h3>Add New Custemer</h3>
-    <input type='text'
-    placeholder='Name'
-    />
-    <button>Submit</button>
+    <input type='text'placeholder='Name' onChange={(e) => setuser(e.target.value)}/>
+    <button onChange={addcusomer}>Submit</button>
     </div>
   )
 }
